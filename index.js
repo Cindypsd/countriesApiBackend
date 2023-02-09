34df9cfc5bd4436834a4081133e616fd48dd02a9
Cindypsd <cindyposada3@gmail.com>
2023-02-09 16:25:58 -0600

    deploy

--- index.js
+++ index.js
@@ -20,11 +20,11 @@
 
 const server = require('./src/app.js');
 const { conn, sequelize } = require('./src/db.js');
-
+const port = process.env.PORT || 3001
 // Syncing all the models at once.
 //          alter
 conn.sync({ ALTER: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
